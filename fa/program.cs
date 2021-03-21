02fa9258aaf0e0198102cfb756dc404ce3d2953a
Igor Kozlov <79710144+starfame@users.noreply.github.com>
2021-03-21 19:34:02 +0300

    Update program.cs

--- fa/program.cs
+++ fa/program.cs
@@ -16,10 +16,48 @@ namespace fans
 
   public class FA1
   {
+    public static State a = new State()
+        {
+            Name = "a",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+        public State b = new State()
+        {
+            Name = "b",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+        public State c = new State()
+        {
+            Name = "c",
+            IsAcceptState = true,
+            Transitions = new Dictionary<char, State>()
+        };
+
+        State InitialState = a;
+        
+        public FA()
+        {
+           a.Transitions['0'] = a;
+           a.Transitions['1'] = b;
+           b.Transitions['0'] = c;
+           b.Transitions['1'] = a;
+           c.Transitions['0'] = b;
+           c.Transitions['1'] = c;            
+        }
     public bool? Run(IEnumerable<char> s)
-    {
-      return false;
-    }
+        {
+            State current = InitialState;
+            foreach (var c in s) // цикл по всем символам 
+            {
+                current = current.Transitions[c]; // меняем состояние на то, в которое у нас переход
+                if (current == null)              // если его нет, возвращаем признак ошибки
+                    return null;
+                // иначе переходим к следующему
+            }
+            return current.IsAcceptState;         // результат true если в конце финальное состояние 
+        }
   }
 
   public class FA2
